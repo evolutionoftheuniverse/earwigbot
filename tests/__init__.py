1035321f57adac191e9a25ee44cc52ac5947aa4e
evolutionoftheuniverse <67762964+evolutionoftheuniverse@users.noreply.github.com>
2020-07-05 09:57:03 +0300

    More more fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -111,7 +111,7 @@ class CommandTestCase(TestCase):
 class FakeBot(Bot):
     def __init__(self, root_dir):
         self.root_dir = root_dir
-        self.config = FakeBotConfig(root_dir, level=20)
+        self.config = FakeBotConfig(self.root_dir, level=20)
         self.logger = logging.getLogger("earwigbot")
         self.commands = CommandManager(self)
         self.tasks = TaskManager(self)
